2750ad7dc26f5513de63b8e2c6f158c860dfd6b4
Kelvin Ding <70010690+kelvin-ding@users.noreply.github.com>
2021-09-19 17:29:38 +0700

    Remove update email
    email byright can't be updated

--- routes/controller/userManagement/update.js
+++ routes/controller/userManagement/update.js
@@ -8,7 +8,6 @@ module.exports = async (req, res) =>{
     //Validate Data
     const schema = {
         name: { type: "string", optional: true, empty:false},
-        email: { type: "email", optional: true, empty:false},
         password: { type: "string", min: 6, optional: true, empty:false} ,
         confirmpassword: { type: "equal", field: "password", optional: true, empty:false },
         role: { type: "string", items: "string", enum: [ "admin", "employee"], optional: true},
@@ -64,7 +63,6 @@ module.exports = async (req, res) =>{
     //Execute query update
     await user.update({
         name: req.body.name,
-        email: req.body.email,
         password: password,
         role: req.body.role,
         status: req.body.status,
